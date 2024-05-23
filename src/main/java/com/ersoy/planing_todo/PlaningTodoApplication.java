cced3f8ea841d9a5141ea3a9945a8500de9ae496
ugur.ersoy@anymile.io <ugur.ersoy@anymile.io>
2024-05-23 13:55:04 +0300

    fist commit

---
+++ src/main/java/com/ersoy/planing_todo/PlaningTodoApplication.java
@@ -0,0 +1,13 @@
+package com.ersoy.planing_todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaningTodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaningTodoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ersoy/planing_todo/PlaningTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ersoy.planing_todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaningTodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
